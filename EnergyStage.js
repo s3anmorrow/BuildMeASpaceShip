a6b25a39528d429d178cc6860421d871eb394fba
Sean Morrow <s3anmorrow@gmail.com>
2016-05-10 13:53:43 -0300

    pickups resetting after going off bottom of stage

--- EnergyStage.js
+++ EnergyStage.js
@@ -1,8 +1,8 @@
 var EnergyStage = function() {
 
     // game stage constants
-    var ENERGY_SPEED = 8;
-    var ENERGY_COUNT = 3;
+    var PICKUP_SPEED = 8;
+    var PICKUP_MAX = 3;
 
     // local references to important globals
     var assetManager = window.assetManager;
@@ -18,22 +18,26 @@ var EnergyStage = function() {
     // event to be dispatched when this stage is complete
     var completeEvent = new createjs.Event("onStageComplete", true);
 
-    // energy balls ready to appear
+    // pickup ready to appear
     var ready = false;
-    // number of energy balls dropped
-    var energyCount = 0;
+    // number of energy balls picked up
+    var pickupCount = 0;
     // current X position of touch
     var touchX = 0;
 
     // master container for this stage's screen
     var screen = new createjs.Container();
     screen.snapToPixelEnabled = true;
-    // container for layering energy balls
-    var energyLayer = new createjs.Container();
+    // container for layering pickup
+    var pickupLayer = new createjs.Container();
 
-    // construct energy sprite
-    var energy = assetManager.getSprite("spacestuff2","energyYellow");
-    energy.active = false;
+    // construct pickup sprite
+    var pickup = assetManager.getSprite("spacestuff2","energy1");
+    pickup.bitmapText = new createjs.BitmapText("",assetManager.getSpriteSheet("interface"));
+    pickup.bitmapText.letterSpacing = 2;
+    pickup.active = false;
+    // construct label sprite
+    var label = assetManager.getSprite("interface","yellow");
 
     // ------------------------------------------------- public methods
     this.showMe = function(){
@@ -52,8 +56,8 @@ var EnergyStage = function() {
         backgroundSprite.addEventListener("mousedown", onMoving);
         backgroundSprite.addEventListener("pressmove", onMoving);
 
-        // add energy layer on top of spaceship
-        screen.addChild(energyLayer);
+        // add pickup layer on top of spaceship
+        screen.addChild(pickupLayer);
 
         root.addChild(screen);
     };
@@ -61,36 +65,67 @@ var EnergyStage = function() {
     this.hideMe = function(){
         // cleanup
         ready = false;
-        energyCount = 0;
+        pickupCount = 0;
         moveDir = 0;
         touchX = 0;
-        energy.active = false;
-        screen.removeChild(energy);
+        pickup.active = false;
+        screen.removeChild(pickup);
         root.removeChild(screen);
     };
 
     this.updateMe = function() {
         if (ready) {
-            // update energy on the screen if active
-            if (energy.active) {
-                energy.y += energy.speed;
-                energy.rotation++;
+            // update pickup on the screen if active
+            if (pickup.active) {
+                pickup.y += pickup.speed;
+                pickup.rotation++;
 
-                // check for pickup on every fourth tick
-                if ((createjs.Ticker.getTicks() % 4) == 0) spaceShip.pickupMe(energy, energyLayer);
+                // collision detection for pickup on every fourth tick
+                if ((createjs.Ticker.getTicks() % 4) == 0) {
+                    // convert pickup x,y relative to shipContainer
+                    var pickupPoint = pickupLayer.localToLocal(pickup.x, pickup.y, spaceShip.getShipContainer());
 
-                // has the energy gone off the bottom of the screen?
-                if (energy.y > baseHeight + 220) {
-                    energy.active = false;
-                    screen.removeChild(energy);
+                    // only if pickup actually on fuselage
+                    if ((pickupPoint.y > 0) && (pickupPoint.y < 400) && (pickupPoint.x > -36) && (pickupPoint.x < 212)) {
+                        console.log("pickup!");  
+                        
+                        // play killed animation
+                        //pickup.gotoAndPlay(pickup.currentAnimation + "Killed");
+                        //pickup.addEventListener("animationend", onPickupKilled);
+                        //pickup.moving = false;
+                        
+                        /*
+                        // setup bitmapText
+                        var bitmapText = asteroid.bitmapText;
+                        // ???????????
+                        bitmapText.text = "";
+                        bitmapText.alpha = 1;
+                        bitmapText.x = asteroid.x - (bitmapText.getBounds().width / 2) - 4;
+                        bitmapText.y = asteroid.y - 44;
+                        asteroidLayer.addChild(bitmapText);
+                        // tween bitmapText fading away
+                        createjs.Tween.get(bitmapText).wait(500).to({alpha:0}, 500).call(onTextFaded);
 
-                    if (energyCount >= ENERGY_COUNT) {
-                        backgroundSprite.removeEventListener("mousedown", onMoving);
-                        backgroundSprite.removeEventListener("pressmove", onMoving);
-                        spaceShip.flyOffStage(onComplete);
-                    } else {
-                        dropComet();
+                        assetManager.getSound("asteroidExplode").play();
+                        */
+                        
                     }
+                    /*
+                    if (!comet.soundPlayed) {
+                        // ?????????
+                        //assetManager.getSound("burn").play();
+                        comet.soundPlayed = true;
+                    }
+                    */
+                    
+                }
+
+                // has the pickup gone off the bottom of the screen?
+                if (pickup.y > baseHeight + 220) {
+                    pickup.active = false;
+                    screen.removeChild(pickup);
+                    // time to drop another pickup?
+                    if (pickupCount < PICKUP_MAX) dropComet();
                 }
             }
             // moving spaceship if required
@@ -104,27 +139,39 @@ var EnergyStage = function() {
     }
 
     function dropComet(e) {
-
-        // setup energy
-        energy.gotoAndPlay("energy" + randomMe(1,3));
-        energy.y = -150;
+        // setup pickup
+        pickup.gotoAndPlay("energy" + randomMe(1,3));
+        pickup.y = -150;
 
         // random horizontal positioning
-        energy.x = randomMe(130,470);
+        pickup.x = randomMe(130,470);
 
-        // setting energy properties
-        energy.speed = ENERGY_SPEED;
-        energy.active = true;
-        energy.soundPlayed = false;
-        screen.addChild(energy);
-        assetManager.getSound("energy").play();
-
-        energyCount++;
+        // setting pickup properties
+        pickup.speed = PICKUP_SPEED;
+        pickup.active = true;
+        pickup.soundPlayed = false;
+        screen.addChild(pickup);
+        //assetManager.getSound("pickup").play();
     }
+    
+    function onPickupKilled(e) {
+        pickupCount++;
+        e.target.stop();
+        e.target.removeAllEventListeners();
+        createjs.Tween.removeTweens(e.target);
+        asteroidLayer.removeChild(e.target);
+        e.target.active = false;
+        // end of stage?
+        if (pickupCount >= PICKUP_MAX) {
+            backgroundSprite.removeEventListener("mousedown", onMoving);
+            backgroundSprite.removeEventListener("pressmove", onMoving);
+            spaceShip.flyOffStage(onComplete);
+        }
+    }    
 
     function onReady(e) {
         ready = true;
-        // drop energy right away
+        // drop pickup right away
         dropComet();
     }
 
